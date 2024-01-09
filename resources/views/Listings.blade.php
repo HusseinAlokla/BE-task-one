fbfae8d9157e0caa816fad6d99a32e275b28a95f
HusseinAlokla <hussein77.alokla@gmail.com>
2024-01-09 19:21:31 +0200

    basic directives

--- resources/views/Listings.blade.php
+++ resources/views/Listings.blade.php
@@ -1,14 +1,18 @@
 <h1>
     {{$heading}}
 </h1>
-<?php foreach ($listings as $listing): ?>
+@if (count($listings)==0)
+<p>No listings found!</p>
+
+@endif
+@foreach ($listings as $listing)
     <h2>
-        <?php echo $listing['title']; ?>
+         {{$listing['title']}}
     </h2>
     <p>
-        <?php echo $listing['description']; ?>
+         {{ $listing['description']}}
     </p>
 
 
 
-<?php endforeach; ?>
+ @endforeach
