c42053f211faa796e129126e0c7e31e906624bf5
HusseinAlokla <hussein77.alokla@gmail.com>
2024-01-10 20:45:54 +0200

    implementing the frontend

--- routes/web.php
+++ routes/web.php
@@ -33,7 +33,13 @@ Route::get('/', function () {
 
 //finding single listing
 Route::get('/listings/{id}', function ($id) {
-    return view('listing', ['listing' => Listing::find($id)]);
+
+    $listing = Listing::find($id);
+    if ($listing) {
+        return view('listing', ['listing' => $listing]);
+    } else {
+        abort(404);
+    }
 });
 
 
